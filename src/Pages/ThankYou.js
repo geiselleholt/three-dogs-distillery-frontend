fd10d312e49f2112b161bb88e81457d2115726ee
Geiselle Holt <geiselleholt@hotmail.com>
2023-02-07 18:44:53 -0600

    Thank You and cancel pages

--- src/Pages/ThankYou.js
+++ src/Pages/ThankYou.js
@@ -1,9 +1,52 @@
 
+import { motion } from "framer-motion";
+import three_dogs_logo from "../images/three_dogs_logo.jpg";
 
 const ThankYou = () => {
   return (
     <div>
-      Thank You For Your Order
+              <h1 className="home__header">
+          <motion.div
+          animate={{rotate: 360}}
+          transition={{ repeat: 2, duration: 3}}
+          >
+            <img alt="spinning logo" src={ three_dogs_logo } width={170} height={120}/>
+          </motion.div>
+          <p className="title">Three Dog's Distillery</p>
+          <section className="header__buttons">
+          <section>
+          <a href="/orderpage">
+            <button className="buttons">Order Now</button>
+          </a>
+        </section>
+        <section>
+          <a href="/">
+            <button className="buttons">Home</button>
+          </a>
+        </section>
+          </section>  
+        </h1>
+      Thank You for Your Order!! Your reciept and order confirmation will be sent to your email. Enjoy your custom made Spirit! Drink responsibly 🥃
+      <footer className="home__footer">
+        <section>
+        <img alt="spinning logo" src={ three_dogs_logo } width={200} height={100}/>
+        </section>
+        <section>
+          <a href="/orderpage">
+            <button className="buttons">Order Now</button>
+          </a>
+        </section>
+        <section>
+          <a href="/contact">
+            <button className="buttons">Contact Nick</button>
+          </a>
+        </section>
+        <section>
+          <a href="/aboutus">
+            <button className="buttons">About Us</button>
+          </a>
+        </section>
+        </footer>
     </div>
   );
 };
